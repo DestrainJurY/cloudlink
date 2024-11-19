8d1bdae919da29d86cce135d2f111394692408ef
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2024-11-18 19:32:28 -0800

    update port

--- server_example.py
+++ server_example.py
@@ -65,4 +65,4 @@ if __name__ == "__main__":
     # server.enable_ssl(certfile="cert.pem", keyfile="privkey.pem")
 
     # Start the server
-    server.run(ip="0.0.0.0", port=3000)
+    server.run(ip="0.0.0.0", port=1944)
